7d4d13d2f37828847d0354e5e62108dae7509d69
Michael Stapelberg <michael@stapelberg.de>
2018-09-30 19:56:40 +0200

    correctly fail the build if the sub-process fails

--- cmd/distri/build.go
+++ cmd/distri/build.go
@@ -488,7 +488,7 @@ func (b *buildctx) build() (runtimedeps []string, _ error) {
 		if err := proto.Unmarshal(b, &meta); err != nil {
 			return nil, err
 		}
-		return meta.GetRuntimeDep(), nil
+		return meta.GetRuntimeDep(), cmd.Wait()
 	}
 
 	logDir := filepath.Dir(b.SourceDir) // TODO: introduce a struct field
