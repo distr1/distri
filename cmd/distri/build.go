06848eeb579baf679f32d91eb7ecc357a3943717
Michael Stapelberg <michael@stapelberg.de>
2018-11-05 08:01:01 +0100

    wrapper scripts: set (mostly) correct $0

--- cmd/distri/build.go
+++ cmd/distri/build.go
@@ -158,6 +158,9 @@ func buildpkg(hermetic, debug, fuse bool) error {
 	return nil
 }
 
+// We need to strip the path from $0 because shebang evaluation doesn’t pass the
+// invoker as $0, but passes the path to the shell script instead.
+// See also https://www.in-ulm.de/~mascheck/various/shebang/
 var wrapperTmpl = template.Must(template.New("").Funcs(template.FuncMap{
 	"quoteenv": func(env string) string {
 		return strings.Replace(env, `=`, `="`, 1) + `"`
@@ -166,7 +169,7 @@ var wrapperTmpl = template.Must(template.New("").Funcs(template.FuncMap{
 {{ range $idx, $env := .Env }}
 export {{ quoteenv $env }}
 {{ end }}
-exec {{ .Prefix }}/{{ .Bin }} "$@"
+exec -a "${0##*/}" {{ .Prefix }}/{{ .Bin }} "$@"
 `))
 
 func (b *buildctx) serialize() (string, error) {
