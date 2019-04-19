9c5a748027ffb37f7b7e582998d76dd039cb6146
Michael Stapelberg <michael@stapelberg.de>
2019-04-19 11:34:58 +0200

    cmakebuilder: enable verbose mode for the entire build

--- cmd/distri/buildcmake.go
+++ cmd/distri/buildcmake.go
@@ -4,8 +4,11 @@ import "github.com/distr1/distri/pb"
 
 func (b *buildctx) buildcmake(opts *pb.CMakeBuilder, env []string) (newSteps []*pb.BuildStep, newEnv []string, _ error) {
 	steps := [][]string{
-		[]string{"/bin/cmake", "${DISTRI_SOURCEDIR}", "-DCMAKE_INSTALL_PREFIX:PATH=${DISTRI_PREFIX}"},
-		[]string{"make", "-j8", "V=1"},
+		[]string{"/bin/cmake", "${DISTRI_SOURCEDIR}",
+			"-DCMAKE_INSTALL_PREFIX:PATH=${DISTRI_PREFIX}",
+			"-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON",
+		},
+		[]string{"make", "-j8"},
 		[]string{"make", "install", "DESTDIR=${DISTRI_DESTDIR}", "PREFIX=${DISTRI_PREFIX}"},
 	}
 	return stepsToProto(steps), env, nil
