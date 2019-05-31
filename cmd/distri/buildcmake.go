5fd28e1002d6f242ab59a59a9e3fa83f104f152f
Michael Stapelberg <michael@stapelberg.de>
2019-05-31 15:25:39 +0200

    plumb through extra cmake flags

--- cmd/distri/buildcmake.go
+++ cmd/distri/buildcmake.go
@@ -4,10 +4,10 @@ import "github.com/distr1/distri/pb"
 
 func (b *buildctx) buildcmake(opts *pb.CMakeBuilder, env []string) (newSteps []*pb.BuildStep, newEnv []string, _ error) {
 	steps := [][]string{
-		[]string{"/bin/cmake", "${DISTRI_SOURCEDIR}",
+		append([]string{"/bin/cmake", "${DISTRI_SOURCEDIR}",
 			"-DCMAKE_INSTALL_PREFIX:PATH=${DISTRI_PREFIX}",
 			"-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON",
-		},
+		}, opts.GetExtraCmakeFlag()...),
 		[]string{"make", "-j8"},
 		[]string{"make", "install", "DESTDIR=${DISTRI_DESTDIR}", "PREFIX=${DISTRI_PREFIX}"},
 	}
