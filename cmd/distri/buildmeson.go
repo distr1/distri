924dddd8f6ed0b36b4df1434284002ce633f54dc
Michael Stapelberg <michael@stapelberg.de>
2019-06-07 17:54:31 +0200

    gofmt buildmeson.go

--- cmd/distri/buildmeson.go
+++ cmd/distri/buildmeson.go
@@ -4,14 +4,15 @@ import "github.com/distr1/distri/pb"
 
 func (b *buildctx) buildmeson(opts *pb.MesonBuilder, env []string) (newSteps []*pb.BuildStep, newEnv []string, _ error) {
 	var steps [][]string
-		steps = append(steps, [][]string{
-			append([]string{
-				"meson",
-				"--prefix=${DISTRI_PREFIX}",
-				".", // build dir
-				"${DISTRI_SOURCEDIR}",
-			}, opts.GetExtraMesonFlag()...),
-		}...)
+	steps = append(steps, [][]string{
+		append([]string{
+			"meson",
+			// TODO: should we set -Drootprefix or is that fuse-specific?
+			"--prefix=${DISTRI_PREFIX}",
+			".", // build dir
+			"${DISTRI_SOURCEDIR}",
+		}, opts.GetExtraMesonFlag()...),
+	}...)
 
 	steps = append(steps, [][]string{
 		[]string{"ninja", "-v"},
