26bfd8c40ab520762cb064d6e4e90371160619b0
Michael Stapelberg <michael@stapelberg.de>
2019-06-09 11:52:30 +0200

    fix scaffold_test

--- cmd/distri/scaffold_test.go
+++ cmd/distri/scaffold_test.go
@@ -1,36 +1,47 @@
 package main
 
-import "testing"
+import (
+	"net/url"
+	"testing"
+)
+
+func mustParse(u string) *url.URL {
+	parsed, err := url.Parse(u)
+	if err != nil {
+		panic(err)
+	}
+	return parsed
+}
 
 func TestNameFromURL(t *testing.T) {
 	for _, tt := range []struct {
-		URL          string
+		URL          *url.URL
 		scaffoldType int
 		wantName     string
 		wantVersion  string
 	}{
 		{
-			URL:          "http://ftp.debian.org/debian/pool/main/w/whois/whois_5.4.2.tar.xz",
+			URL:          mustParse("http://ftp.debian.org/debian/pool/main/w/whois/whois_5.4.2.tar.xz"),
 			scaffoldType: scaffoldC,
 			wantName:     "whois",
 			wantVersion:  "5.4.2",
 		},
 
 		{
-			URL:          "https://xorg.freedesktop.org/releases/individual/lib/libXxf86vm-1.1.4.tar.bz2",
+			URL:          mustParse("https://xorg.freedesktop.org/releases/individual/lib/libXxf86vm-1.1.4.tar.bz2"),
 			scaffoldType: scaffoldC,
 			wantName:     "libxxf86vm",
 			wantVersion:  "1.1.4",
 		},
 
 		{
-			URL:          "https://github.com/traviscross/mtr/archive/v0.92.tar.gz",
+			URL:          mustParse("https://github.com/traviscross/mtr/archive/v0.92.tar.gz"),
 			scaffoldType: scaffoldC,
 			wantName:     "mtr",
 			wantVersion:  "0.92",
 		},
 	} {
-		t.Run(tt.URL, func(t *testing.T) {
+		t.Run(tt.URL.String(), func(t *testing.T) {
 			name, version, err := nameFromURL(tt.URL, tt.scaffoldType)
 			if err != nil {
 				t.Fatalf("nameFromURL: %v", err)
