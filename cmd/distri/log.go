246b5b8c00654ab6ed5b359a0eac30235fafd72b
Michael Stapelberg <michael@stapelberg.de>
2019-06-09 23:36:29 +0200

    distri log: default to most recent log, add -version flag
    Now that we have package revision comparison, this is easy :)

--- cmd/distri/log.go
+++ cmd/distri/log.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 	"syscall"
 
+	"github.com/distr1/distri"
 	"github.com/distr1/distri/internal/env"
 	"golang.org/x/xerrors"
 )
@@ -16,25 +18,33 @@ const logHelp = `TODO
 
 func showlog(args []string) error {
 	fset := flag.NewFlagSet("log", flag.ExitOnError)
-	var ()
+	var (
+		version = fset.String("version", "", "package version (default: most recent)")
+	)
 	fset.Parse(args)
 	if fset.NArg() != 1 {
 		return xerrors.Errorf("syntax: log <package>")
 	}
 	pkg := fset.Arg(0)
 
-	matches, err := filepath.Glob(filepath.Join(env.DistriRoot, "build", pkg, "*.log"))
-	if err != nil {
-		return err
-	}
-	if got, want := len(matches), 1; got != want {
-		return xerrors.Errorf("unexpected number of logfiles: got %d, want %d (matches: %v)", got, want, matches)
+	var match string
+	if *version != "" {
+		match = filepath.Join(env.DistriRoot, "build", pkg, "build-"+*version+".log")
+	} else {
+		matches, err := filepath.Glob(filepath.Join(env.DistriRoot, "build", pkg, "*.log"))
+		if err != nil {
+			return err
+		}
+		sort.Slice(matches, func(i, j int) bool {
+			return distri.PackageRevisionLess(matches[j], matches[i]) // reverse
+		})
+		match = matches[0]
 	}
 
 	shargs := []string{
 		"/bin/sh",
 		"-c",
-		fmt.Sprintf("${PAGER:-less} %q", matches[0]),
+		fmt.Sprintf("${PAGER:-less} %q", match),
 	}
 	return syscall.Exec("/bin/sh", shargs, os.Environ())
 }
