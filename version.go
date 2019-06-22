f071889ca541c89f7a50373488474f844801480a
Michael Stapelberg <michael@stapelberg.de>
2019-06-22 10:14:28 +0200

    version: refactor version parsing into ParseVersion

--- version.go
+++ version.go
@@ -5,22 +5,68 @@ import (
 	"strings"
 )
 
-// extractPackageRevision returns the distri package revision (e.g. 37 for
-// glibc-amd64-2.27-37) from the specified filename. When no package revision
-// can be parsed, 0 is returned.
-func extractPackageRevision(filename string) int64 {
+// PackageVersion describes one released version of a package. It is assumed
+// that files never change in the archive, but may become unavailable.
+type PackageVersion struct {
+	// Upstream is the upstream version number. It is never parsed or compared,
+	// and is meant for human consumption only.
+	Upstream string
+
+	// DistriRevision is an incrementing integer starting at 1. Every time the
+	// package is changed, it must be increased by 1 so that e.g. distri update
+	// will see the package. Even if upstream versions change, the revision does
+	// not reset. E.g., 8.2.0-3 could be followed by 8.3.0-4.
+	//
+	// If the version could not be parsed, DistriRevision is 0.
+	DistriRevision int64
+}
+
+func (pv *PackageVersion) String() string {
+	return pv.Upstream + "-" + strconv.FormatInt(pv.DistriRevision, 10)
+}
+
+var fileExtensions = map[string]bool{
+	"squashfs":       true,
+	"meta.textproto": true,
+	"log":            true,
+}
+
+func buildFile(filename string) bool {
+	parts := strings.Split(filename, "/")
+	return parts[len(parts)-1] == "build"
+}
+
+// ParseVersion constructs a PackageVersion from filename,
+// e.g. glibc-amd64-2.27-37, which parses into PackageVersion{Upstream: "2.27",
+// DistriRevision: 37}.
+func ParseVersion(filename string) PackageVersion {
 	parts := strings.Split(filename, "-")
 	// Discard everything up to the architecture identifier, including the first
 	// minus-separated part following it (the upstream version).
 	for i := 1; i < len(parts); i++ {
 		if Architectures[parts[i]] {
-			// TODO: bounds check
-			parts = parts[i+2:]
+			// Skip all remaining architecture parts (e.g. in
+			// gcc-i686-amd64-8.2.0).
+			for Architectures[parts[i]] && i < len(parts) {
+				i++
+			}
+			parts = parts[i:]
 			break
 		}
 	}
 	if len(parts) == 0 {
-		return 0
+		return PackageVersion{}
+	}
+	upstream := strings.Join(parts, "-")
+	// TODO: make build log files contain the architecture and delete this conditional:
+	if buildFile(parts[0]) {
+		upstream = strings.Join(parts[1:], "-")
+	}
+	for ext := range fileExtensions {
+		upstream = strings.TrimSuffix(upstream, "."+ext)
+	}
+	if len(parts) == 1 {
+		return PackageVersion{Upstream: upstream}
 	}
 	rev := parts[len(parts)-1]
 	if idx := strings.IndexByte(rev, '.'); idx > -1 {
@@ -30,14 +76,22 @@ func extractPackageRevision(filename string) int64 {
 		rev = rev[:idx] // constrain ourselves to this path component
 	}
 	revision, _ := strconv.ParseInt(rev, 0, 64)
-	return revision
+	if revision > 0 {
+		upstream = strings.Join(parts[:len(parts)-1], "-")
+		// TODO: make build log files contain the architecture and delete this conditional:
+		if buildFile(parts[0]) {
+			upstream = strings.Join(parts[1:len(parts)-1], "-")
+		}
+
+	}
+	return PackageVersion{Upstream: upstream, DistriRevision: revision}
 }
 
 // PackageRevisionLess returns true if the distri package revision extracted
 // from filenameA is less than those extracted from filenameB. This can be used
 // with sort.Sort.
 func PackageRevisionLess(filenameA, filenameB string) bool {
-	versionA := extractPackageRevision(filenameA)
-	versionB := extractPackageRevision(filenameB)
+	versionA := ParseVersion(filenameA).DistriRevision
+	versionB := ParseVersion(filenameB).DistriRevision
 	return versionA < versionB
 }
--- version_test.go
+++ version_test.go
@@ -5,35 +5,60 @@ import "testing"
 func TestExtractPackageRevisionVersion(t *testing.T) {
 	for _, tt := range []struct {
 		filename string
-		want     int64
+		want     PackageVersion
 	}{
 		{
 			filename: "less-amd64-530",
-			want:     0,
+			want:     PackageVersion{Upstream: "530", DistriRevision: 0},
+		},
+
+		{
+			filename: "530",
+			want:     PackageVersion{Upstream: "530", DistriRevision: 0},
+		},
+
+		{
+			filename: "530-3",
+			want:     PackageVersion{Upstream: "530", DistriRevision: 3},
+		},
+
+		{
+			filename: "v0.0.0-20180314180146-1d60e4601c6f",
+			want:     PackageVersion{Upstream: "v0.0.0-20180314180146-1d60e4601c6f"},
+		},
+
+		{
+			filename: "gcc-i686-amd64-8.2.0-3.squashfs",
+			want:     PackageVersion{Upstream: "8.2.0", DistriRevision: 3},
+		},
+
+		{
+			filename: "gcc-i686-amd64-8.2.0.squashfs",
+			want:     PackageVersion{Upstream: "8.2.0", DistriRevision: 0},
 		},
 
 		{
 			filename: "less-amd64-530-2",
-			want:     2,
+			want:     PackageVersion{Upstream: "530", DistriRevision: 2},
 		},
 
 		{
 			filename: "less-amd64-530-17.squashfs.gz",
-			want:     17,
+			want:     PackageVersion{Upstream: "530", DistriRevision: 17},
 		},
 
 		{
 			filename: "../less-amd64-530-17/bin/less", // exchange dir link target
-			want:     17,
+			want:     PackageVersion{Upstream: "530", DistriRevision: 17},
 		},
 
 		{
 			filename: "build/git/build-2.9.5-3.log", // build log
-			want:     3,
+			want:     PackageVersion{Upstream: "2.9.5", DistriRevision: 3},
 		},
 	} {
 		t.Run(tt.filename, func(t *testing.T) {
-			got := extractPackageRevision(tt.filename)
+			got := ParseVersion(tt.filename)
 			if got != tt.want {
 				t.Fatalf("extractVersion(%v) = %v, want %v", tt.filename, got, tt.want)
 			}
