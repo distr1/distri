b055bdb38d7c9d039acf0df89c4254e755ff91e0
Michael Stapelberg <michael@stapelberg.de>
2019-06-27 21:48:20 +0200

    bump: implement bumping individual package(s)
    This bumps the transitive closure of reverse dependencies, i.e. all packages
    which could possibly be affected by changes to the specified package(s).

--- cmd/distri/bump.go
+++ cmd/distri/bump.go
@@ -14,6 +14,9 @@ import (
 	"github.com/distr1/distri/pb"
 	"github.com/golang/protobuf/proto"
 	"github.com/google/renameio"
+	"golang.org/x/xerrors"
+	"gonum.org/v1/gonum/graph/simple"
+	"gonum.org/v1/gonum/graph/topo"
 )
 
 const bumpHelp = `distri bump [-flags] [package]
@@ -81,6 +84,196 @@ func bumpAll(write bool) ([]versionIncrement, error) {
 	return inc, nil
 }
 
+type bumpctx struct {
+	graph      *simple.DirectedGraph
+	arch       string
+	cnt        int64
+	byFullname map[string]*bumpnode // e.g. gcc-amd64-8.2.0
+	byPkg      map[string]*bumpnode // e.g. gcc
+	srcCache   map[string]string    // pkg → src, e.g. gcc-libs → gcc
+	globCache  map[string][]string  // TODO: remove in favor of a single pass through env.DefaultRepo
+	bumped     map[string]bool
+}
+
+type bumpnode struct {
+	id int64
+
+	pkg      string // e.g. make
+	version  string // TODO: remove fullname
+	fullname string // package and version, e.g. make-4.2.1
+	deps     []string
+}
+
+func (n *bumpnode) ID() int64 { return n.id }
+
+func (b *bumpctx) srcOf(pkg string) (string, error) {
+	if src, ok := b.srcCache[pkg]; ok {
+		return src, nil
+	}
+	m, err := pb.ReadMetaFile(filepath.Join(env.DefaultRepo, pkg+".meta.textproto"))
+	if err != nil {
+		return "", err
+	}
+	src := pkg
+	if sp := m.GetSourcePkg(); sp != "" {
+		src = sp
+	}
+	b.srcCache[pkg] = src
+	return src, nil
+}
+
+func (b *bumpctx) addPkg(pkg string) error {
+	buildTextprotoPath := filepath.Join(env.DistriRoot, "pkgs", pkg, "build.textproto")
+	c, err := ioutil.ReadFile(buildTextprotoPath)
+	if err != nil {
+		return err
+	}
+	var buildProto pb.Build
+	if err := proto.UnmarshalText(string(c), &buildProto); err != nil {
+		return err
+	}
+	fullname := pkg + "-" + b.arch + "-" + buildProto.GetVersion()
+	if _, ok := b.byFullname[fullname]; ok {
+		return nil // already added
+	}
+	//log.Printf("adding %s", pkg)
+	b.cnt++
+	n := &bumpnode{
+		id:       b.cnt,
+		pkg:      pkg,
+		fullname: fullname,
+		version:  buildProto.GetVersion(),
+	}
+	b.byPkg[n.pkg] = n
+	b.byPkg[n.pkg+"-"+b.arch] = n
+	b.byFullname[n.fullname] = n
+	b.graph.AddNode(n)
+
+	{
+		deps := buildProto.GetDep()
+		bld := &buildctx{Arch: "amd64"} // TODO
+		deps = append(deps, bld.builderdeps(&buildProto)...)
+		deps = append(deps, buildProto.GetRuntimeDep()...)
+		srcs := make([]string, 0, len(deps))
+		for _, r := range deps {
+			g, ok := b.globCache[r]
+			if !ok {
+				var err error
+				g, err = bld.glob(env.DefaultRepo, []string{r})
+				if err != nil {
+					continue // build.textproto present, but no build artifact
+				}
+				b.globCache[r] = g
+			}
+			src, err := b.srcOf(g[0])
+			if err != nil {
+				return err
+			}
+			srcs = append(srcs, src)
+		}
+		//log.Printf("srcs = %v", srcs)
+		n.deps = srcs
+	}
+
+	return nil
+}
+
+func newBumpctx() (*bumpctx, error) {
+	b := &bumpctx{
+		// TODO: use simple.NewDirectedMatrix instead?
+		graph:      simple.NewDirectedGraph(),
+		arch:       "amd64", // TODO: configurable / auto-detect
+		byFullname: make(map[string]*bumpnode),
+		byPkg:      make(map[string]*bumpnode),
+		srcCache:   make(map[string]string),
+		globCache:  make(map[string][]string),
+		bumped:     make(map[string]bool),
+	}
+	d, err := os.Open(filepath.Join(env.DistriRoot, "pkgs"))
+	if err != nil {
+		return nil, err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return nil, err
+	}
+	d.Close()
+	//var inc []versionIncrement
+	for _, pkg := range names {
+		if err := b.addPkg(pkg); err != nil {
+			return nil, err
+		}
+	}
+	// All nodes added, add edges between nodes
+	nodes := b.graph.Nodes()
+	for nodes.Next() {
+		n := nodes.Node().(*bumpnode)
+		for _, dep := range n.deps {
+			if dep == n.fullname ||
+				dep == n.pkg+"-"+b.arch ||
+				dep == n.pkg {
+				continue // skip adding self edges
+			}
+			if d, ok := b.byFullname[dep]; ok {
+				b.graph.SetEdge(b.graph.NewEdge(n, d))
+			}
+			if d, ok := b.byPkg[dep]; ok {
+				b.graph.SetEdge(b.graph.NewEdge(n, d))
+			}
+		}
+	}
+
+	// Break cycles
+	if _, err := topo.Sort(b.graph); err != nil {
+		uo, ok := err.(topo.Unorderable)
+		if !ok {
+			return nil, err
+		}
+		for _, component := range uo { // cyclic component
+			//log.Printf("uo %d", idx)
+			for _, n := range component {
+				//log.Printf("  bootstrap %v", n.(*bumpnode).pkg)
+				from := b.graph.From(n.ID())
+				for from.Next() {
+					b.graph.RemoveEdge(n.ID(), from.Node().ID())
+				}
+			}
+		}
+		if _, err := topo.Sort(b.graph); err != nil {
+			return nil, xerrors.Errorf("could not break cycles: %v", err)
+		}
+	}
+
+	return b, nil
+}
+
+func (b *bumpctx) bumpPkg(pkg string) ([]versionIncrement, error) {
+	if b.bumped[pkg] {
+		return nil, nil
+	}
+	n := b.byPkg[pkg]
+	nodes := b.graph.To(n.id)
+	inc := []versionIncrement{
+		versionIncrement{
+			pkg:     n.pkg,
+			current: n.version,
+			new:     incrementVersion(n.version),
+		},
+	}
+	b.bumped[pkg] = true
+	for nodes.Next() {
+		n := nodes.Node().(*bumpnode)
+		tmp, err := b.bumpPkg(n.pkg)
+		if err != nil {
+			return nil, err
+		}
+		inc = append(inc, tmp...)
+	}
+
+	return inc, nil
+}
+
 func bump(args []string) error {
 	fset := flag.NewFlagSet("bump", flag.ExitOnError)
 	var (
@@ -98,7 +291,22 @@ func bump(args []string) error {
 			return err
 		}
 	} else {
-		// TODO: verify flag.Args() contains a package name
+		if fset.NArg() == 0 {
+			fmt.Fprintln(os.Stderr, "syntax: bump [-flags] <package> [<package>...]")
+			os.Exit(2)
+		}
+
+		b, err := newBumpctx()
+		if err != nil {
+			return err
+		}
+		for _, arg := range fset.Args() {
+			tmp, err := b.bumpPkg(arg)
+			if err != nil {
+				return err
+			}
+			inc = append(inc, tmp...)
+		}
 	}
 	if *write {
 		for _, i := range inc {
--- integration/bump/bump_test.go
+++ integration/bump/bump_test.go
@@ -11,11 +11,40 @@ import (
 	"github.com/golang/protobuf/proto"
 )
 
-const buildTextproto = `
+var files = map[string]string{
+	"pkgs/test/build.textproto": `
 source: "empty://"
 hash: ""
 version: "530-1"
-`
+
+dep: "systemd"
+`,
+
+	"pkgs/lvm2/build.textproto": `
+source: "empty://"
+hash: ""
+version: "2.03.00-3"
+`,
+
+	"pkgs/systemd/build.textproto": `
+source: "empty://"
+hash: ""
+version: "239-7"
+
+dep: "lvm2"
+`,
+
+	"pkgs/unaffected/build.textproto": `
+source: "empty://"
+hash: ""
+version: "1-1"
+`,
+
+	"build/distri/pkg/test-amd64-530-1.meta.textproto":     `source_pkg: "test"`,
+	"build/distri/pkg/lvm2-amd64-2.03.00-3.meta.textproto": `source_pkg: "lvm2"`,
+	"build/distri/pkg/systemd-amd64-239-7.meta.textproto":  `source_pkg: "systemd"`,
+	"build/distri/pkg/unaffected-amd64-1-1.meta.textproto": `source_pkg: "unaffected"`,
+}
 
 func TestBump(t *testing.T) {
 	t.Parallel()
@@ -27,19 +56,21 @@ func TestBump(t *testing.T) {
 	defer os.RemoveAll(distriroot)
 
 	// Write package build instructions:
-	pkgDir := filepath.Join(distriroot, "pkgs", "test")
-	if err := os.MkdirAll(pkgDir, 0755); err != nil {
-		t.Fatal(err)
-	}
-	if err := ioutil.WriteFile(
-		filepath.Join(pkgDir, "build.textproto"),
-		[]byte(buildTextproto),
-		0644); err != nil {
-		t.Fatal(err)
+	for path, content := range files {
+		fullpath := filepath.Join(distriroot, path)
+		if err := os.MkdirAll(filepath.Dir(fullpath), 0755); err != nil {
+			t.Fatal(err)
+		}
+		if err := ioutil.WriteFile(
+			fullpath,
+			[]byte(content),
+			0644); err != nil {
+			t.Fatal(err)
+		}
 	}
 
 	build := exec.Command("distri", "bump", "-all", "-w")
-	build.Dir = pkgDir
+	build.Dir = filepath.Join(distriroot, "pkgs/test")
 	build.Env = []string{"DISTRIROOT=" + distriroot}
 	build.Stdout = os.Stdout
 	build.Stderr = os.Stderr
@@ -48,7 +79,7 @@ func TestBump(t *testing.T) {
 	}
 
 	var bld pb.Build
-	b, err := ioutil.ReadFile(filepath.Join(pkgDir, "build.textproto"))
+	b, err := ioutil.ReadFile(filepath.Join(distriroot, "pkgs/test/build.textproto"))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -57,5 +88,61 @@ func TestBump(t *testing.T) {
 	}
 	if got, want := bld.GetVersion(), "530-2"; got != want {
 		t.Errorf("bump: got %q, want %q", got, want)
+	}
+}
+
+func TestBumpRdeps(t *testing.T) {
+	t.Parallel()
+
+	distriroot, err := ioutil.TempDir("", "integrationbump")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(distriroot)
+
+	// Write package build instructions:
+	for path, content := range files {
+		fullpath := filepath.Join(distriroot, path)
+		if err := os.MkdirAll(filepath.Dir(fullpath), 0755); err != nil {
+			t.Fatal(err)
+		}
+		if err := ioutil.WriteFile(
+			fullpath,
+			[]byte(content),
+			0644); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	build := exec.Command("distri", "bump", "-w", "lvm2")
+	build.Env = []string{"DISTRIROOT=" + distriroot}
+	build.Stdout = os.Stdout
+	build.Stderr = os.Stderr
+	if err := build.Run(); err != nil {
+		t.Fatalf("%v: %v", build.Args, err)
+	}
+
+	for _, tt := range []struct {
+		pkg  string
+		want string
+	}{
+		{pkg: "lvm2", want: "2.03.00-4"},
+		{pkg: "systemd", want: "239-8"},
+		{pkg: "test", want: "530-2"},
+		{pkg: "unaffected", want: "1-1"},
+	} {
+		t.Run(tt.pkg, func(t *testing.T) {
+			var bld pb.Build
+			b, err := ioutil.ReadFile(filepath.Join(distriroot, "pkgs", tt.pkg, "build.textproto"))
+			if err != nil {
+				t.Fatal(err)
+			}
+			if err := proto.UnmarshalText(string(b), &bld); err != nil {
+				t.Fatal(err)
+			}
+			if got, want := bld.GetVersion(), tt.want; got != want {
+				t.Errorf("bump: got %q, want %q", got, want)
+			}
+		})
 	}
 }
