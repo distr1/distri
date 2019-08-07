5a1b79a7e81fdc62b084520bf3c888018b51c009
Michael Stapelberg <michael@stapelberg.de>
2019-08-07 11:34:50 +0200

    autobuilder: build docs, too

--- cmd/autobuilder/autobuilder.go
+++ cmd/autobuilder/autobuilder.go
@@ -75,6 +75,7 @@ var steps = []step{
 	{"image-serial", []string{"sh", "-c", "mkdir -p $DESTDIR/img && make image serial=1 DISKIMG=$DESTDIR/img/distri-qemu-serial.img"}},
 	{"image-gce", []string{"sh", "-c", "mkdir -p $DESTDIR/img && make gceimage GCSDISKIMG=$DESTDIR/img/distri-gce.tar.gz"}},
 	// TODO(later): docker
+	{"docs", []string{"sh", "-c", "make docs DOCSDIR=$DESTDIR/docs"}},
 
 	{"cp-destdir", []string{"sh", "-c", "cp --link -r -f -a build/distri/* $DESTDIR/"}},
 }
